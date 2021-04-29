b64f4e4ad482aae3903a556481c8001020265b76
Badho Bharat <82494076+badhobharat@users.noreply.github.com>
2021-04-29 16:05:22 +0530

    Create code.php

---
+++ code.php
@@ -0,0 +1,13 @@
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
+function logout_without_confirm($action, $result)
+{
+    /**
+     * Allow logout without confirmation
+     */
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) {
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : 'https://badhobharat.in/';
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));
+        header("Location: $location");
+        die;
+    }
+}
